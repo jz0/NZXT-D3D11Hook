90df9509fe61abe86ba3a32bc97471f619c4ea06
j <49915210+jz0@users.noreply.github.com>
2023-01-09 02:24:14 +0200

    Update Entry.cpp

--- NZXT-D3D11Hook/Entry.cpp
+++ NZXT-D3D11Hook/Entry.cpp
@@ -12,8 +12,8 @@ void Init()
 	uintptr_t D3D11Present = Util::FindPattern("Graphics Hook64.dll", "40 57 48 83 EC 30 48 C7 44 24 20 FE FF FF FF 48 89 5C 24 40 48 89 6C 24 48 48 89 74 24 58 41 8B F0 8B EA 48 8B D9 41 0F B6 F8 40 80 E7 01");
 	printf("[+] D3D11Present %p \n", D3D11Present);
 
-	//Create IDXGISwapChain::Present hook via the hooked MinHook 
-	//Hookception ¯\_(-___-)_/¯
+	//Create IDXGISwapChain::Present hook via the hijacked MinHook 
+	//Hookception Â¯\_(-___-)_/Â¯
 	if (Hooks::MH_CreateHook(D3D11Present, (uintptr_t)Hooks::Present, (uintptr_t)&OriginalPresent))
 	{
 		//Enable hooks
